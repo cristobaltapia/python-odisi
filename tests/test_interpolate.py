4f509c74dc853190061f9150caeaa7510505dcdc
Cristóbal Tapia Camú <crtapia@gmail.com>
2023-12-04 14:58:32 +0100

    test: add test for interpolation with rerative time

--- tests/test_interpolate.py
+++ tests/test_interpolate.py
@@ -7,13 +7,13 @@ from odisi import read_tsv
 
 
 class TestInterpolation:
-    def test_sync_time(self):
+    def test_interp_time_dataframe(self):
         data_full = read_tsv("tests/data/verification_data_ch1_full.tsv")
         data_time = pl.read_csv(
             "tests/data/verification_load.csv", try_parse_dates=True
         )
 
-        time = data_time.select(["time [s]"])[::2, :]
+        time = data_time.select(["time [s]"])[::2]
         new_time = data_full.interpolate(time)
         r = [-0.9, 0.8, 4.1, 4.0, 7.4333333]
         # Assert the correctness of the interpolation
@@ -21,7 +21,7 @@ class TestInterpolation:
         # Assert the new rate
         assert data_full.rate == 0.4
 
-    def test_sync_time_array(self):
+    def test_interp_time_array(self):
         data_full = read_tsv("tests/data/verification_data_ch1_full.tsv")
         data_time = pl.read_csv(
             "tests/data/verification_load.csv", try_parse_dates=True
@@ -29,6 +29,20 @@ class TestInterpolation:
 
         time = data_time.select(["time [s]"]).to_series().to_numpy()[::2]
         new_time = data_full.interpolate(time)
+        r = [-0.9, 0.8, 4.1, 4.0, 7.4333333]
+        # Assert the correctness of the interpolation
+        assert_almost_equal(new_time[3, 1:6].to_numpy()[0], r)
+        # Assert the new rate
+        assert data_full.rate == 0.4
+
+    def test_interp_time_relative(self):
+        data_full = read_tsv("tests/data/verification_data_ch1_full.tsv")
+        data_time = pl.read_csv(
+            "tests/data/verification_load_relative_time.csv", try_parse_dates=True
+        )
+
+        time = data_time.select(["time [s]"]).to_series().to_numpy()[::2]
+        new_time = data_full.interpolate(time, relative_time=True)
         r = [-0.9, 0.8, 4.1, 4.0, 7.4333333]
         # Assert the correctness of the interpolation
         assert_almost_equal(new_time[3, 1:6].to_numpy()[0], r)
